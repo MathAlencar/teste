6dafe5e549592203e93b502cd152aca86d8e62a4
Matheus Nascimento <matheus.nascimento@stefani.local>
2025-01-23 10:29:48 -0300

    teste

--- route.js
+++ route.js
@@ -3,6 +3,5 @@ const routes = express.Router()
 const func = require('./controllers/validacoes')
 
 routes.get('/validacao/:numero', func.validando_numero)
-routes.post('/reunindo/dados', func.validando_numero)
 
 module.exports = routes
